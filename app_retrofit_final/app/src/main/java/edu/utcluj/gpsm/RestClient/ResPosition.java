8eef0ca8e3ae1fcbe853fab0414e619d4ed44a3a
BOGDAN-LAPTOP\Bogdan <bogdanneag@ymail.com>
2019-11-10 15:33:48 +0200

    Android Retrofit Final

---
+++ app_retrofit_final/app/src/main/java/edu/utcluj/gpsm/RestClient/ResPosition.java
@@ -0,0 +1,55 @@
+package edu.utcluj.gpsm.RestClient;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ResPosition {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("latitude")
+    @Expose
+    private String latitude;
+
+    @SerializedName("longitude")
+    @Expose
+    private String longitude;
+
+    @SerializedName("terminalId")
+    @Expose
+    private String terminalId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(String terminalId) {
+        this.terminalId = terminalId;
+    }
+}
